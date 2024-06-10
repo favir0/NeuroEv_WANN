cb73d308f9a8dc4fe9b7933dc5d5846519c2f594
favir0 <f4vir0@yandex.ru>
2024-06-10 23:08:13 +0700

    feat: add base feed-forward nn class

---
+++ ff_nn.py
@@ -0,0 +1,11 @@
+class NeuralNetwork:
+    def __init__(self, genome):
+        self.genome = genome
+        self.nodes = {}
+        self.connections = []
+    
+    def feed():
+        ...
+    
+    def visualize():
+        ...
