bd41a2e39083ebe72b4d0aa5d767b2df7c0b9f0d
favir0 <f4vir0@yandex.ru>
2024-06-11 17:36:42 +0700

    feat: add mutation genome mutations

--- wann_core.py
+++ wann_core.py
@@ -1,3 +1,5 @@
+import random 
+
 from activation import ActivationF
 
 
@@ -64,3 +66,86 @@ class Genome:
         )
         self.connections[connection.connection_id] = connection
         return connection
+    
+    def mutation_add_connection(self):
+        potential_connections = []
+        for node_i in self.nodes:
+            for node_j in self.nodes:
+                if (
+                    node_i.layer < node_j.layer
+                    and (node_i.node_id, node_j.node_id)
+                    not in self.connections
+                ):
+                    potential_connections.append((node_i, node_j))
+
+        if not potential_connections:
+            return
+
+        in_node, out_node = random.choice(potential_connections)
+        weight = config.wann_initial_weight
+        self.add_connection(in_node, out_node, weight)
+
+    def mutation_split_connection(self):
+        possible_connections = []
+        for connection in self.connections.values():
+            new_layer = (connection.from_node.layer + connection.to_node.layer) // 2
+            if new_layer != connection.from_node.layer:
+                possible_connections.append(connection)
+
+        if not possible_connections:
+            return
+
+        connection = random.choice(possible_connections)
+        node = self.add_node(layer=connection.from_node.layer + 1)
+        self.add_connection(in_node=connection.from_node, out_node=node, weight=config.wann_initial_weight)
+        self.add_connection(in_node=node, out_node=connection.to_node, weight=connection.weight)
+
+    def mutation_disable_connection(self):
+        possible_connections = list(filter(lambda x: x.enabled, self.connections.values()))
+
+        if not possible_connections:
+            return
+
+        connection = random.choice(list(possible_connections))
+        connection.enabled = False
+
+    def mutation_enable_connection(self):
+        possible_connections = list(filter(lambda x: not x.enabled, self.connections.values()))
+
+        if not possible_connections:
+            return
+
+        connection = random.choice(list(possible_connections))
+        connection.enabled = True
+
+    def mutation_change_activation_f(self):
+        node = random.choice(self.nodes)
+        activation_f = random.choice(list(ActivationF))
+        node.activation_f = activation_f
+
+    def mutation_change_activation_response(self):
+        node = random.choice(self.nodes)
+        response = node.activation_response + random.uniform(
+            -config.max_activation_response_delta,
+            config.max_activation_response_delta,
+        )
+        node.activation_response = np.clip(response, *config.activation_response_range)
+
+    def mutate(self):
+        if random.random() < config.mutation_disable_connection_prob:
+            self.mutation_disable_connection()
+        if random.random() < config.mutation_enable_connection_prob:
+            self.mutation_enable_connection()
+        if random.random() < config.mutation_change_activation_f_prob:
+            self.mutation_change_activation_f()
+        if random.random() < config.mutation_change_activation_response_prob:
+            self.mutation_change_activation_response()
+
+        if random.random() < config.mutation_add_connection_prob:
+            self.mutation_add_connection()
+            if config.single_structure_mutation:
+                return
+        if random.random() < config.mutation_split_connection_prob:
+            self.mutation_split_connection()
+            if config.single_structure_mutation:
+                return
